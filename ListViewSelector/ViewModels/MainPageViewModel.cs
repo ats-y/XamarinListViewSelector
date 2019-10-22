338a257805a044d6d67df3d3e52fc8366f510bb7
atsushi <london.rightside@gmail.com>
2019-10-22 16:22:12 +0900

    Prism化。

---
+++ ListViewSelector/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ListViewSelector.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel()
+        {
+        }
+    }
+}
